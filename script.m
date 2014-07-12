58fe51daf9a135c4063316c75f775a32362712c3
N-Mavros <ncmavros@gmail.com>
2014-07-12 11:19:59 -0400

    Not yet optimized, includes C coefficient

--- script.m
+++ script.m
@@ -21,20 +21,20 @@ problem.MaxIterations 	= 10000;
 problem.print = 1;
 problem.solver = 'IPOPT';		% use this solver
 
-problem.N = 20;					% use this number of nodes (optim will reduce this to number of gait samples, if needed
+problem.N = 50;					% use this number of nodes (optim will reduce this to number of gait samples, if needed
 
 % lower and upper bounds on some of the variables we are optimizing (set lower=upper if variable should not be optimized)
 problem.L_u1 = 0.001;		problem.U_u1 = 1.0; 		% uncomment this line to keep valve 1 closed
 problem.L_u2 = 0.001;		problem.U_u2 = 1.0; 		% valve 2 control
 problem.L_k  = 0.1;			problem.U_k  = 12;			% spring stiffness in MPa/ml
-problem.L_u3 = -50.0;       problem.U_u3 = 100.0;        %(NM-added u3 lower and upper bounds to the problem statement)
+problem.L_u3 = -50.0;       problem.U_u3 = 50.0;        %(NM-added u3 lower and upper bounds to the problem statement)
 
 % weights for cost function
-problem.w1 				= 10.;			% weight for angle tracking term
+problem.w1 				= 10.0;			% weight for angle tracking term
 problem.w2				= 1.0;			% weight for moment tracking term
 problem.w3 				= 0.1;			% weight for valve 1 control accelerations
 problem.w4 				= 0.1;			% weight for valve 2 control accelerations
-problem.w5              = 0.01;          % weight for the pump controller (NM)
+problem.w5              = 0.05;          % weight for the pump controller (NM)
 problem.wreg 			= 0.0;			% weight for regularization (smoo thness) term
 problem.Wdisc 			= 0.0;			% weighting to encourage discrete control levels
 problem.Ndisc 			= 2.0;			% number of discrete control levels (dont touch)
@@ -48,6 +48,7 @@ problem.C2max		= 30;			% Cmax for valve 2
 problem.B1 			= 0.01;			% viscous drag coefficient for valve 1 & its tubing
 problem.B2 			= 0.01;			% viscous drag coefficient for valve 2 & its tubing
 problem.B3          = 0.01;         % viscous drag coefficient for pump and hardware
+problem.C           = 0;            % coefficient for parallel accumulator
 	
 % do the optimization	
 result = optim(problem);
